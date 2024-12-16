a2599182a2e94aa0eb7ab22daa11d4043652287f
Connor Meredith <connor.meredith0913@gmail.com>
2024-12-16 13:07:34 -0500

    Add comments

--- ExportConverter/src/main/scala/io/github/cmeredit/game_map/Mask.scala
+++ ExportConverter/src/main/scala/io/github/cmeredit/game_map/Mask.scala
@@ -1,44 +1,88 @@
 package io.github.cmeredit.game_map
 
+// Provides convenience methods for mask data stored as a 1-Dim container of numerical values
 case class Mask(data: Vector[Short], xDim: Int, yDim: Int, zDim: Int) {
 
-  val xRowSizeShorts: Int = xDim / 16
-  val xyLayerSizeShorts: Int = xRowSizeShorts * yDim
+  // Adding this in case I want to change what numeric type is used later, but I doubt it.
+  // At a minimum, it gives a name to a random number.
+  //
+  // The reason I'm using shorts is that DF maps are composed of chunks of 16x16 tiles, and each
+  // map can have 3 or more chunks in any direction. I.e., 16 is the gcd of all possible map
+  // sizes (xDim's and yDim's). I would really like each x-row to be a whole number of
+  // data carriers, and shorts are the largest numeric type to meet this requirement.
+  private val shortSizeBits = 16
 
+  // I keep getting "map size in tiles" mixed up with "map size when compressed into shorts" mixed
+  // up while coding. Adding in some clearly named variables to make this easier.
+  //
+  // Number of shorts that encode a complete row of tiles on the map x-axis.
+  val xRowSizeShorts: Int = xDim / 16
+  // Number of shorts that encode a complete grid of tiles on the map xy-plane.
+  // I.e., number of shorts per z layer
+  val xyLayerSizeShorts: Int = xRowSizeShorts * yDim
+  // Number of bits that encode a complete row of tiles on the map x-axis.
+  // Since we have one bit per tile, this is just an alias for xDim
   val xRowSizeBits: Int = xDim
+  // Number of bits that encode a complete grid of tiles on the map xy-plane.
+  // Since we have one bit per tile, this is just the number of tiles per zLayer, i.e., xDim*yDim.
   val xyLayerSizeBits: Int = xRowSizeBits * yDim
 
 
+  // Gets the mask value from the specified map coordinates.
   def getBit(x: Int, y: Int, z: Int): Int = {
 
+    // Conceptually, the data is as a collection of xy layers, from lowest to highest z index.
+    // Each xy layer is stored as a collection of x rows, from lowest to highest y index.
+    //
+    // I.e., the data array was filled using a loop like this:
+    //
+    // for z = 0 until zDim
+    //   for y = 0 until yDim
+    //     for x = 0 until xDim
+    //       [Append mask bit from coordinate x, y, z to data buffer]
+    //     end for
+    //   end for
+    // end for
+    //
+    // Convert tile coordinates to bit index based on the aforementioned storage scheme
     val bitIndex: Int = z * xyLayerSizeBits + y * xRowSizeBits + x
-    val shortIndex: Int = bitIndex / 16
-
+    // Convert bit index to an index of the data array (i.e., index of the short that contains the desired bit)
+    val shortIndex: Int = bitIndex / shortSizeBits
     val targetShort: Short = data(shortIndex)
 
-    val bitIndexWithinShort: Int = bitIndex % 16
+    // We've already passed by shortIndex * shortSizeBits bits. How much further do we need to go?
+    val bitIndexWithinShort: Int = bitIndex % shortSizeBits
 
-    // Shift the target bit all the way to the right, then mask to it.
-    // Note that the bitIndexWithinShort is between 0 and 15.
+    // Shift the target bit all the way to the right, then mask to it. Signed-right-bit-shift smears the sign bit. E.g., (0b1000 >> 3) == 0b1111, not 0b0001.
+    // Note that the bitIndexWithinShort is between 0 and 15 (shortSizeBits-1).
     // For example, if we're retrieving bit 15, then it's already at the least significant digit, so don't need to shift
     // Most significant digit (index 0) would need to be shifted 15 bits.
-    val targetBit = (targetShort >> (15 - bitIndexWithinShort)) & 1
+    val targetBit = (targetShort >> ((shortSizeBits-1) - bitIndexWithinShort)) & 1
 
     targetBit
   }
 
+  // Convenience function for interpreting mask entries as booleans instead of bits.
   def getBoolean(x: Int, y: Int, z: Int): Boolean = {
     getBit(x, y, z) == 1
   }
 
+  // Convenience functions for applying element-wise operations between masks.
+  // I only define a single unary operation, which just flips each mask bit.
   def neg(): Mask = Mask(data.map(s => (~s).toShort), xDim, yDim, zDim)
+  // Standard logical operations and applyOp, which makes it easier to define nonstandard ops as well.
+  // For example, you could write something like m1.implies(m2) as m1.applyOp(m2, (s1, s2) => (~s1 | s2).toShort).
   def applyOp(other: Mask, op: (Short, Short) => Short): Mask = Mask(data.zip(other.data).map({case (s1, s2) => op(s1, s2)}), xDim, yDim, zDim)
   def or(other: Mask): Mask = applyOp(other, (s1, s2) => (s1 | s2).toShort)
   def and(other: Mask): Mask = applyOp(other, (s1, s2) => (s1 & s2).toShort)
   def xor(other: Mask): Mask = applyOp(other, (s1, s2) => (s1 ^ s2).toShort)
   def iff(other: Mask): Mask = (this xor other).neg()
 
-
+  // Returns a copy of the current mask that has been shifted up one z coordinate / xy layer.
+  // The copy does not contain the highest xy layer of the original mask.
+  // The bottom xy layer in the copy is determined by the extension method argument.
+  // For example, with method "AllTrue", the bottom xy layer of the returned mask will have all its bits set.
+  // As another example, with the method "CopyBoundary", the bottom xy layer of the returned mask is the same as in the original mask.
   def shiftedUpZ(extensionMethod: BoundaryOptions.BoundaryExtensionMethod): Mask = {
     import BoundaryOptions._
     extensionMethod match {
@@ -53,6 +97,9 @@ case class Mask(data: Vector[Short], xDim: Int, yDim: Int, zDim: Int) {
     }
   }
 
+  // Returns a copy of the current mask that has been shifted down one z coordinate / xy layer.
+  // The copy does not contain the lowest xy layer of the original mask.
+  // The highest xy layer in the copy is determined by the extension method argument.
   def shiftedDownZ(extensionMethod: BoundaryOptions.BoundaryExtensionMethod): Mask = {
     import BoundaryOptions._
     extensionMethod match {
@@ -67,6 +114,9 @@ case class Mask(data: Vector[Short], xDim: Int, yDim: Int, zDim: Int) {
     }
   }
 
+  // Returns a copy of the current mask that has been shifted up one y coordinate / xz layer.
+  // The copy does not contain the highest xz layer of the original mask.
+  // The lowest xz layer in the copy is determined by the extension method argument.
   def shiftedUpY(extensionMethod: BoundaryOptions.BoundaryExtensionMethod): Mask = {
     import BoundaryOptions._
 
@@ -86,6 +136,9 @@ case class Mask(data: Vector[Short], xDim: Int, yDim: Int, zDim: Int) {
 
   }
 
+  // Returns a copy of the current mask that has been shifted down one y coordinate / xz layer.
+  // The copy does not contain the lowest xz layer of the original mask.
+  // The highest xz layer in the copy is determined by the extension method argument.
   def shiftedDownY(extensionMethod: BoundaryOptions.BoundaryExtensionMethod): Mask = {
     import BoundaryOptions._
 
