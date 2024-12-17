7a577ae3e0879b4744cba63a39d4c598b53faa6b
Connor Meredith <connor.meredith0913@gmail.com>
2024-12-17 05:36:58 -0700

    Write obj saving function

--- ExportConverter/src/main/scala/io/github/cmeredit/mesh/Mesh.scala
+++ ExportConverter/src/main/scala/io/github/cmeredit/mesh/Mesh.scala
@@ -1,5 +1,27 @@
 package io.github.cmeredit.mesh
 
+import java.io.{BufferedWriter, FileWriter}
+
 case class Mesh(faces: Vector[Face]) {
+
+  def saveToObj(filename: String, scale: Double): Unit = {
+
+    val vertexIndexMap: Map[Vertex, Int] = faces.flatMap(_.vertices).distinct.zipWithIndex.toMap
+
+    val outFile: BufferedWriter = new BufferedWriter(new FileWriter(filename))
+
+    vertexIndexMap.foreach({case (Vertex(x, y, z), _) =>
+      outFile.write(f"v ${x.toDouble * scale} ${y.toDouble * scale} ${z.toDouble * scale}")
+    })
+
+    faces.foreach({case Face(vertices) =>
+      val vertexLabels = vertices.map(v => (vertexIndexMap(v) + 1).toString)
+      val vertexLabelLine = vertexLabels.reduce(_ + " " + _)
+      outFile.write(f"f $vertexLabelLine")
+    })
+
+    outFile.close()
+
+  }
 
 }
