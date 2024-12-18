5c178ecd3f3435b5b99e1801e3d7ff6d8c124cfb
Connor Meredith <connor.meredith0913@gmail.com>
2024-12-18 15:12:02 -0700

    Add vertex perturbation and convenience method for creating squares (given an axis, shift direction, and base point)

--- ExportConverter/src/main/scala/io/github/cmeredit/mesh/Face.scala
+++ ExportConverter/src/main/scala/io/github/cmeredit/mesh/Face.scala
@@ -1,3 +1,31 @@
 package io.github.cmeredit.mesh
 
+import io.github.cmeredit.Orientations._
+
 case class Face(vertices: Vector[Vertex])
+
+object Face {
+  def squareFromAxisWithUpwardsOffset(orientation: Orientation, basePoint: Vertex): Face = {
+    // Order in which to perturb the base point to achieve a CC-oriented face
+    val standardPerturbationAxes = orientation.axis match {
+      case X => Vector(Y, Z)
+      case Y => Vector(Z, X)
+      case Z => Vector(X, Y)
+    }
+
+    val ourAxes = if (orientation.upwards) standardPerturbationAxes else standardPerturbationAxes.reverse
+
+    val faceVerts: Vector[Vertex] = Vector(
+      basePoint,
+      basePoint.perturbed(ourAxes.take(1)),
+      basePoint.perturbed(ourAxes),
+      basePoint.perturbed(ourAxes.takeRight(1))
+    )
+
+    if (orientation.upwards)
+      Face(faceVerts.map(_.perturbed(Vector(orientation.axis))))
+    else
+      Face(faceVerts)
+
+  }
+}
--- ExportConverter/src/main/scala/io/github/cmeredit/mesh/Vertex.scala
+++ ExportConverter/src/main/scala/io/github/cmeredit/mesh/Vertex.scala
@@ -1,3 +1,12 @@
 package io.github.cmeredit.mesh
 
-case class Vertex(x: Int, y: Int, z: Int)
+import io.github.cmeredit.Orientations._
+
+case class Vertex(x: Int, y: Int, z: Int) {
+
+  def perturbed(perturbationDirections: Vector[Axis]): Vertex = {
+    def offset(axis: Axis): Int = if (perturbationDirections.contains(axis)) 1 else 0
+    Vertex(x + offset(X), y + offset(Y), z + offset(Z))
+  }
+
+}
